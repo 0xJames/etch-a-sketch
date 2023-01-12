2228f224a6d3405555ffbabe1d566432dc483440
James Yang <jy0xcs@gmail.com>
2023-01-11 21:24:33 -0800

    Created square using CSS.

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+const square = document.createElement('div')
+
+square.style.height= '50px'
+square.style.width = "50px"
+square.style.backgroundColor ="blue"
+
+//for (let i = 0; i <17; i++){
+  container.appendChild(square)
+  container.appendChild(square)
+  
+
+//} clone nodes and append them?
