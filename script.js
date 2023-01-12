a63334e797ede555c520688110479c4480b7c140
James Yang <jy0xcs@gmail.com>
2023-01-12 15:15:47 -0800

    Learnt basic CSS grid layout.

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const container = document.querySelector('#container');
-const square = document.createElement('div')
+//const container = document.querySelector('#container');
+//const square = document.createElement('div')
 
-square.style.height= '50px'
-square.style.width = "50px"
-square.style.backgroundColor ="blue"
+//square.style.height= '50px'
+//square.style.width = "50px"
+//square.style.backgroundColor ="blue"
 
-//for (let i = 0; i <17; i++){
-  container.appendChild(square)
-  container.appendChild(square)
+
+
+  //container.appendChild(square)
   
-
-//} clone nodes and append them?
+  
